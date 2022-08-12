e8fd5227c29ccddbc070b8189b37f60cdf40dbee
Bernabe19 <bernasanchezsos4@gmail.com>
2022-08-12 11:42:20 +0200

    a

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ var cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
+app.use("/public",express.static(__dirname + "public"));
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
